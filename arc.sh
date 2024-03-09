7200f1a9114b6827fc15628d7b03df97a06be66b
bo-jr <seongbo5230@gmail.com>
2024-03-08 17:49:41 -0800

    put a basic readme in there

---
+++ arc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAMESPACE="arc-systems"
+helm install arc \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
